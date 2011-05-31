b98ef2617b5992b6401cabca09b5bb362c2b7865
siva <sivasakthi.m@gmail.com>
2011-05-31 23:00:44 +0530

    micropost added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110531164238) do
+ActiveRecord::Schema.define(:version => 20110531171811) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
